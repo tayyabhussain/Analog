463cb9f02ace647bad60e2661fccf45d7fbc24ec
tayyab.hussain <tayyab.hussain@coeus-solutions.de>
2014-10-17 18:10:08 +0500

    images added

--- clock.js
+++ clock.js
@@ -16,7 +16,7 @@ Math.TowPi = 2 * Math.PI;
 */
 function displayTime() {
    var now = new Date();
-    var h = now.getHours()+1;
+    var h = now.getHours();
     var m = now.getMinutes();
     var s = now.getSeconds();
     
@@ -28,7 +28,7 @@ function displayTime() {
         var targetX = clockX + Math.cos(armRadians) * (armLength * clockRadius);
         var targetY = clockY + Math.sin(armRadians) * (armLength * clockRadius);
         
-        context.lineWidth = armThickness;
+      //  context.lineWidth = armThickness;
         context.strokeStyle = armColor;
  
         context.beginPath();
@@ -53,16 +53,30 @@ function addDots(){
         var armRadians = (Math.TowPi * t) - (Math.TowPi/4);
         var targetX = clockX + Math.cos(armRadians) * (1 * clockRadius);
         var targetY = clockY + Math.sin(armRadians) * (1 * clockRadius);
-        if(s%5==0){
-            context.lineWidth = 3;
+        var img=new Image();
+        
+        if(s==15){
+            img.src= './folder/3.png';
+            context.drawImage(img, targetX-6, targetY-7, 15, 15);
+        }
+        else if(s==30){
+            img.src= './folder/6.png';
+            context.drawImage(img, targetX-6, targetY-7, 15, 15);
+        }
+        else if(s==45){
+            img.src= './folder/9.png';
+            context.drawImage(img, targetX-6, targetY-7, 15, 15);
+        }
+        else if(s==60){
+            img.src= './folder/12.png';
+            context.drawImage(img, targetX-6, targetY-7, 15, 15);
         }
         else{
-            context.lineWidth = 1;
-        }
         context.strokeStyle = 'black';
         context.beginPath();
-        context.arc(targetX,targetY,2,0,Math.TowPi);
+        context.arc(targetX,targetY,1,0,Math.TowPi);
         context.stroke();
+        }
     }
 }
 
@@ -77,4 +91,3 @@ clockApp();
 
 
  
-
